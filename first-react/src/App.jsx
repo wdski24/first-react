26ffe530070a3c73b2559ba2a187a143953ecc11
wdski <wdski24b@jonas-heuer.com>
2025-07-01 13:06:19 +0200

    useState

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,23 +1,51 @@
 //Import CSS to file, to use classes
+import { useState } from 'react';
 import './App.css';
+import react from './assets/react.svg';
 import HeadingAndButton from './components/HeadingAndButton/HeadingAndButton';
 
 //Functional Component -> Name der Funktion ist identisch zum Namen des Files und beginnt immer mit einem großen Buchstaben
 function App() {
   //Jedes Return-Statement darf nur genau ein Element zurück geben
   //<></> : React Fragment -> Wird beim Rendering komplett entfernt
+  //UseState: State bleibt über den Renderzyklus bestehen und wird wie eine variable verwendet werden. Besteht aus dem Wert und einer Setter Funktion
+  const [counter, setCounter] = useState(0);
+  const buttonClick = () => {
+    console.log('function clicked');
+    //Funktion innerhalb der Setter-Funktion notwendig, wenn auf den aktuellen Wert des States zurückgegriffen werden soll, ansonsten kann direkt der gewünschte Wert geschrieben werde
+    setCounter((a) => a + 1);
+  };
   return (
     <>
       <div>
         <h1>Site Title</h1>
         <button className='button'>Button</button>
       </div>
+      <img src={react} />
       <div>
         <h2>Bigger Heading</h2>
-        <HeadingAndButton />
-        <HeadingAndButton />
-        <HeadingAndButton />
-        <HeadingAndButton />
+        {/* React fasst die Attribute zusammen zu einem Object (props-Object) mit key-value Paaren*/}
+        <HeadingAndButton
+          heading='Heading 1'
+          text='text 1'
+          buttonText='button 1'
+          buttonClick={buttonClick}
+        />
+        <HeadingAndButton
+          heading='Heading 2'
+          text='text 2'
+          buttonText='button 2'
+        />
+        <HeadingAndButton
+          heading='Heading 3'
+          text='text 3'
+          buttonText='button 3'
+        />
+        <HeadingAndButton
+          heading='Heading 4'
+          text='text 4'
+          buttonText='button 4'
+        />
       </div>
       <div>
         <img />
@@ -29,6 +57,10 @@ function App() {
           natus voluptates, dicta laboru
         </p>
       </div>
+      {/* React Render Zyklus: Wenn in einer Komponente ein Update stattfindet, werden alle Childkomponenten und die Komponente selbst neu gerendert
+       * <button onClick={setCounter}>{counter}</button>
+       */}
+      <button onClick={buttonClick}>{counter}</button>
     </>
   );
 }
--- first-react/src/components/HeadingAndButton/HeadingAndButton.jsx
+++ first-react/src/components/HeadingAndButton/HeadingAndButton.jsx
@@ -27,16 +27,24 @@ const HeadingAndButton = () => {
 export default HeadingAndButton
 */
 //Styled Component
-
-const HeadingAndButton = () => {
+//props = properties object = übergebene Parameter an Komponente
+/*
+* props = {
+    key 1: '',
+    key2: '',
+  }
+*{key1, key2} ist die sogenannte Object Deconstruction. Ich nenne die Keys eines Objects und kann sie dann direkt verwenden
+*/
+const HeadingAndButton = ({ heading, text, buttonText, buttonClick }) => {
   const headingStyle = {
     fontSize: '12px',
   };
   return (
+    //Binding eines Wertes an einen Tag
     <div>
-      <h2 style={headingStyle}>HeadingAndButton</h2>
-      <p>Lorem Ipsum alsdkjflaöksdjflökasjdcmlökjadäsojf</p>
-      <button>Button</button>
+      <h2 style={headingStyle}>{heading}</h2>
+      <p>{text}</p>
+      <button onClick={buttonClick}>{buttonText}</button>
     </div>
   );
 };
